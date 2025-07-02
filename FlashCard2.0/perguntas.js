4c6ba90355294976c403e449d57551dcebd8fd09
IgorQuintilis <igor.quintiliano.conceicao@escola.pr.gov.br>
2025-07-01 23:53:36 -0300

    Primeira versão do meu projeto
    Primeira versão do meu projeto

---
+++ FlashCard2.0/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Scratch',
+    'O que é o Scratch?',
+    ' É uma linguagem de programação visual que usa blocos para ensinar lógica de programação de forma simples e divertida.'
+)
+
+criaCartao(
+    'JavaScript',
+    'O que é JavaScript?',
+    ' É uma linguagem de programação usada para criar páginas web interativas e dinâmicas.'
+)
+
+criaCartao(
+    'P5.js',
+    'O que é p5.js?',
+    ' Uma biblioteca JavaScript voltada para arte, visualizações e interações com o usuário.'
+)
+
+criaCartao(
+    'Programação Geral',
+    'O que é algoritmo?',
+    'É um conjunto ordenado de passos para resolver um problema específico.'
+)
